e21b00ffdda7616a6ef5ba7647b904f07405f121
Raalsky <raalsky@gmail.com>
2020-06-04 22:17:13 +0200

    Structure refactored

--- toxic/__init__.py
+++ toxic/__init__.py
@@ -1,16 +1,16 @@
 import tensorflow as tf
 
 
-class ToxicClassifier:
-    def __init__(self):
+def preapre_environment():
+    physical_devices = tf.config.list_physical_devices('GPU')
+    # print(physical_devices)
+
+    try:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        # print('Tensorflow sets memory gropth to True')
+    except:
+        # Invalid device or cannot modify virtual devices once initialized.
         pass
 
-    def predict(self, sequences):
-        return {
-            'scores': [
-                {
-                    'score': 0.75,
-                    'toxic': True
-                } for seq in sequences
-            ]
-        }
+
+preapre_environment()
